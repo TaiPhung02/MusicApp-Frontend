38c4f16e9a99fec3644b0eff47955c3bec4d5cfc
TaiDev <2051063445@e.tlu.edu.vn>
2025-03-28 13:56:56 +0700

    fix: safe-area-inset-bottom for MusicPlayer

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,9 @@ const App = () => {
       </div>
 
       {activeSong?.id && (
-        <div className="absolute h-20 bottom-0 left-0 right-0 flex animate-slideup bg-[#1b191f] border-t border-solid border-[#555257] z-10">
+        <div
+          className="absolute h-20 bottom-0 left-0 right-0 flex animate-slideup bg-[#1b191f] border-t border-solid border-[#555257] z-10"
+          style={{ paddingBottom: "env(safe-area-inset-bottom)" }}>
           <MusicPlayer />
         </div>
       )}
